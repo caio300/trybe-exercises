65bbab257e9d19aa35a053f600647b063b3027c8
Caio <caioqs.info@gmail.com>
2020-11-17 11:30:13 -0300

    Adicionar frase

--- exercises/2.1/script.js
+++ exercises/2.1/script.js
@@ -1,4 +1,5 @@
 let pessoaEstudante = "euMesmo";
 let hasbilidadesPessoaEstudante = 10;
 //Eu sou Tryber e vou ter muito sucesso na programação!
+//Eu sou Tryber e vou ter uma carreira próspera !
 console.log("sucesso");
