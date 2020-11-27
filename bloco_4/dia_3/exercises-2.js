c5fb9f7de3b415f48187eadefc3cea2e8e373757
Caio <caioqs.info@gmail.com>
2020-11-27 18:36:38 -0300

    Feito exercicio 2

---
+++ bloco_4/dia_3/exercises-2.js
@@ -0,0 +1,10 @@
+// Para o segundo exercício, faça o mesmo que antes, mas que imprima um triângulo retângulo com 5 asteriscos de base. Por exemplo:
+
+let n = 5;
+
+let l = "*";
+
+for (let index = 1; index <= 5; index += 1) {
+    console.log(l.repeat(index));
+    
+}
