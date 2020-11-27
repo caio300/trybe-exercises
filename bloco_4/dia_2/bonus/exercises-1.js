36fb49a255c3641a4f9c15dc676e61e9465aff6e
Caio <caioqs.info@gmail.com>
2020-11-27 12:28:30 -0300

    Feito exercicio bonus 1

---
+++ bloco_4/dia_2/bonus/exercises-1.js
@@ -0,0 +1,24 @@
+// Ordene o array numbers em ordem crescente e imprima seus valores;
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+console.log("Desordenado.");
+
+console.log(numbers);
+
+for (let index = 1; index < numbers.length; index += 1) {
+    for (jindex = 0; jindex < index; jindex += 1) {
+
+        if (numbers[index] < numbers[jindex]) {
+            let position = numbers[index];
+
+            numbers[index] = numbers[jindex];
+            numbers[jindex] = position;
+
+        }
+    }
+}
+
+console.log("Ordem crescente.");
+
+console.log(numbers);
